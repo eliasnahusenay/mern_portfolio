b4c7707e09341c0deaa454b5e4d9cd92e8cecabf
eliasnahusenay <elias.nahusenay@tutors.es>
2025-02-12 15:43:29 +0200

    feat: Implement backend models

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true, // Ensure email is unique
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true } // Adds createdAt and updatedAt fields
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
